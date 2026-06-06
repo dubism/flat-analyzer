5d53c26860c17d67903e935e1aa28f55b76b8f03
dubism <m.v.dubis@gmail.com>
2026-06-06 10:25:24 +0200

    Route flat notes as second app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import FlatNotesApp from './FlatNotesApp';
 import './index.css';
+
+function Root() {
+  const [hash, setHash] = useState(() => window.location.hash || '');
+
+  useEffect(() => {
+    const handleHashChange = () => setHash(window.location.hash || '');
+    window.addEventListener('hashchange', handleHashChange);
+    return () => window.removeEventListener('hashchange', handleHashChange);
+  }, []);
+
+  if (hash.startsWith('#/notes')) return <FlatNotesApp />;
+  return <App />;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
